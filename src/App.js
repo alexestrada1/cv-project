5f12be7e8170f1a4c4cd921255003d49bc667f54
alexestrada1 <estradaalex46@gmail.com>
2023-03-07 18:22:59 -0600

    Added generalInfo functionality and it saves in its own state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from "react"
 import GeneralInfo from "./Components/GeneralInfo";
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-    };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state)
   }
-
-
+  handleFieldsChange = fields =>{
+    this.setState(fields)
+  }
   render(){
     return(
     <div className="App">
-    <form>
-      <GeneralInfo />
+    <form onSubmit={this.handleSubmit}>
+      <GeneralInfo onChange={this.handleFieldsChange}/>
+      <button type="submit">Submit CV</button>
     </form>
     </div> 
     )
--- src/Components/GeneralInfo.js
+++ src/Components/GeneralInfo.js
@@ -1,21 +1,40 @@
 import React from "react";
 
- class GeneralInfo extends React.Component {
+class GeneralInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: ""
+    };
+  }
+  
+  handleChange = e =>{
+    const {id, value} = e.target;
+    this.setState({
+      [id]: value,
+    })
+    this.props.onChange(this.state)
+  }
+
   render() {
+    const {name, email, phone} = this.state
+
     return (
       <div className="gen-info">
         <label htmlFor="name">Enter name: </label>
-        <input type="text" id="name" ></input>
+        <input type="text" id="name" value={name} onChange = {this.handleChange}></input>
 
         <label htmlFor="email">Email: </label>
-        <input type="email" id="email" ></input>
+        <input type="email" id="email" value={email} onChange = {this.handleChange}></input>
 
         <label htmlFor="phone">Phone Number: </label>
-        <input type="tel" id="phone" ></input>
+        <input type="tel" id="phone" value={phone} onChange = {this.handleChange}></input>
 
-        <button type="submit">Submit</button>
       </div>
     );
   }
 }
-export default GeneralInfo
+export default GeneralInfo;
