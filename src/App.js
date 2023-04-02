f4e815c0eadf2fbbe877c5782472d69c9e29ef3f
alexestrada1 <estradaalex46@gmail.com>
2023-04-02 17:16:43 -0500

    added submit button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import GeneralInfo from "./Components/GeneralInfo";
 import WorkExp from "./Components/WorkExperience";
 import SchoolExp from "./Components/SchoolExperience";
@@ -7,55 +7,90 @@ import Overview from "./Components/Overview";
 const App = () => {
   const [showWorkExpForm, setShowWorkExpForm] = useState(false);
   const [showSchoolForm, setShowSchoolExpForm] = useState(false);
-  const[workExp, setWorkExp] = useState([]);
-  const[schoolExp, setSchoolExp] = useState([]);
-  const [generalInfo, setGeneralInfo] = useState('')
+  const [workExp, setWorkExp] = useState([]);
+  const [schoolExp, setSchoolExp] = useState([]);
+  const [generalInfo, setGeneralInfo] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const submitForm = (e) => {
     e.preventDefault();
     setShowWorkExpForm(false);
     setShowSchoolExpForm(false);
-
-    //console.log({state});
+    setIsSubmitted(true);
   };
+
   const handleGeneralInfoChange = (fields) => {
     setGeneralInfo(fields);
   };
+
   const submitWorkExp = (workExpData) => {
     const updatedWorkExperiences = [...workExp, workExpData];
-    setWorkExp(updatedWorkExperiences)
+    setWorkExp(updatedWorkExperiences);
   };
-  const handleToggleWorkForm = () =>{
-    setShowWorkExpForm(!showWorkExpForm)
-  }
-  const handleHideWorkForm = () =>{
-    setShowWorkExpForm(false)
-  }
+
+  const handleToggleWorkForm = () => {
+    setShowWorkExpForm(!showWorkExpForm);
+  };
+
+  const handleHideWorkForm = () => {
+    setShowWorkExpForm(false);
+  };
 
   const submitSchoolExp = (schoolExpData) => {
-    const updatesSchoolExp = [ ...schoolExp, schoolExpData ];
+    const updatesSchoolExp = [...schoolExp, schoolExpData];
     setSchoolExp(updatesSchoolExp);
   };
-  const handleToggleSchoolForm = () =>{
-    setShowSchoolExpForm(!showSchoolForm)
-  }
-  const handleHideSchoolForm = () =>{
-    setShowSchoolExpForm(false)
 
-  }
-  const handleDeleteWork = (index) =>{
+  const handleToggleSchoolForm = () => {
+    setShowSchoolExpForm(!showSchoolForm);
+  };
+
+  const handleHideSchoolForm = () => {
+    setShowSchoolExpForm(false);
+  };
+
+  const handleDeleteWork = (index) => {
     const newArray = [...workExp];
     newArray.splice(index, 1);
-    setWorkExp(newArray );
-}
-const handleDeleteSchool = (index) =>{
-  const newArray = [...schoolExp];
-  newArray.splice(index, 1);
-  setSchoolExp(newArray);
-}
-    return (
-      <div className="App">
-        <form onSubmit={submitForm}>
+    setWorkExp(newArray);
+  };
+
+  const handleDeleteSchool = (index) => {
+    const newArray = [...schoolExp];
+    newArray.splice(index, 1);
+    setSchoolExp(newArray);
+  };
+
+  return (
+    <div className="App">
+      {isSubmitted ? (
+        <>
+          <h2>General Information</h2>
+          <p>Name: {generalInfo.name}</p>
+          <p>Email: {generalInfo.email}</p>
+          <p>Phone: {generalInfo.phone}</p>
+
+          <h2>Work Experience</h2>
+          {workExp.map((exp, index) => (
+            <div key={index}>
+              <p>Company: {exp.company}</p>
+              <p>Title: {exp.jobTitle}</p>
+              <p>Years Worked: {exp.yearsWorked}</p>
+              <button onClick={() => handleDeleteWork(index)}>Delete</button>
+            </div>
+          ))}
+
+          <h2>School Experience</h2>
+          {schoolExp.map((exp, index) => (
+            <div key={index}>
+              <p>School: {exp.school}</p>
+              <p>GraduationYear: {exp.graduationYear}</p>
+              <button onClick={() => handleDeleteSchool(index)}>Delete</button>
+            </div>
+          ))}
+        </>
+      ) : (
+        <>
           <GeneralInfo onChange={handleGeneralInfoChange} />
           <br />
           <h2>Work Experience</h2>
@@ -69,8 +104,9 @@ const handleDeleteSchool = (index) =>{
           <SchoolExp schoolExp={schoolExp} onSubmit={submitSchoolExp} showSchoolForm={showSchoolForm} onHideSchoolForm={handleHideSchoolForm}
           />
           <button onClick={submitForm}>Submit CV</button>
-        </form>
-      </div>
-    );
-}
-export default App;
+        </>
+      )}
+    </div>
+  );
+};
+export default App
